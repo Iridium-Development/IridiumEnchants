9468e59d0e0da4a3802cfbbfecd585ed8d078d0c
Daniel Scherf <munita.gavai@gmail.com>
2024-09-04 23:31:14 +0200

    Fix import

--- src/main/java/com/iridium/iridiumenchants/effects/VeinMine.java
+++ src/main/java/com/iridium/iridiumenchants/effects/VeinMine.java
@@ -1,6 +1,6 @@
 package com.iridium.iridiumenchants.effects;
 
-import com.iridium.iridiumcore.dependencies.xseries.XMaterial;
+import com.cryptomorin.xseries.XMaterial;
 import com.iridium.iridiumcore.utils.StringUtils;
 import com.iridium.iridiumenchants.IridiumEnchants;
 
